3e2b562b9839b3d8299889e4e2d0fa247cfbc7b3
mulwa <mulwatech@gmail.com>
2018-03-16 23:04:40 +0300

    added error handling installed body-parser

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const userRoutes = require('./api/routes/Users');
 
+app.use(bodyParser.urlencoded({ extended:false}));
+app.use(bodyParser.json());
 
 app.use('/user',userRoutes);
+// handling  error for wrong endpoint
+app.use((req,res,next)=>{
+    const error = new Error('End point Not  Found');
+    error.status = 400;
+    next(error);
+});
+app.use((error,req,res,next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error: error.message
+    });
+})
 module.exports = app;
